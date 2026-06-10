0c429dcc2c08c5ed2398b048b41da4cfdff592a2
B.Yaswanth durga bose <yaswanthbheemereddi@gmail.com>
2026-06-10 10:13:25 +0530

    Add gallery photo: ashok (ID: 10)

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,64 @@
 // GALLERY_DATA_START
 const galleryItems = [
-  {"id":1,"title":"Single Person Blood Art","cat":"blood-art","img":"assets/blood_art.png"},
-  {"id":2,"title":"Wedding Couple Golden Border","cat":"frames","img":"assets/frame_wedding.png"},
-  {"id":3,"title":"Double Person Blood Art","cat":"blood-art","img":"assets/blood_art.png"},
-  {"id":4,"title":"Collage Family Frame Layout","cat":"frames","img":"assets/frame_collage.png"},
-  {"id":5,"title":"Heat Activated Reveal","cat":"gifts","img":"assets/premium_mug.png"},
-  {"id":6,"title":"Bespoke Walnut Frame Custom","cat":"frames","img":"assets/frame_wooden.png"},
-  {"id":7,"title":"Acrylic Floating Board Frame","cat":"frames","img":"assets/frame_acrylic.png"},
-  {"id":8,"title":"Brushed Gunmetal Frame","cat":"frames","img":"assets/frame_metal.png"},
-  {"id":9,"title":"Traditional Ornate Gold Frame","cat":"frames","img":"assets/frame_wedding.png"}
+  {
+    "id": 1,
+    "title": "Single Person Blood Art",
+    "cat": "blood-art",
+    "img": "assets/blood_art.png"
+  },
+  {
+    "id": 2,
+    "title": "Wedding Couple Golden Border",
+    "cat": "frames",
+    "img": "assets/frame_wedding.png"
+  },
+  {
+    "id": 3,
+    "title": "Double Person Blood Art",
+    "cat": "blood-art",
+    "img": "assets/blood_art.png"
+  },
+  {
+    "id": 4,
+    "title": "Collage Family Frame Layout",
+    "cat": "frames",
+    "img": "assets/frame_collage.png"
+  },
+  {
+    "id": 5,
+    "title": "Heat Activated Reveal",
+    "cat": "gifts",
+    "img": "assets/premium_mug.png"
+  },
+  {
+    "id": 6,
+    "title": "Bespoke Walnut Frame Custom",
+    "cat": "frames",
+    "img": "assets/frame_wooden.png"
+  },
+  {
+    "id": 7,
+    "title": "Acrylic Floating Board Frame",
+    "cat": "frames",
+    "img": "assets/frame_acrylic.png"
+  },
+  {
+    "id": 8,
+    "title": "Brushed Gunmetal Frame",
+    "cat": "frames",
+    "img": "assets/frame_metal.png"
+  },
+  {
+    "id": 9,
+    "title": "Traditional Ornate Gold Frame",
+    "cat": "frames",
+    "img": "assets/frame_wedding.png"
+  },
+  {
+    "id": 10,
+    "title": "ashok",
+    "cat": "frames",
+    "img": "assets/gallery/gallery_1781066586155.png"
+  }
 ];
 // GALLERY_DATA_END
